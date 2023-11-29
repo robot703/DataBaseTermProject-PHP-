0f1bf633c85f1f213181e3c3733c976f93866994
minjae <robot70345@gmail.com>
2023-11-30 00:59:37 +0900

    style login, register

--- login.php
+++ login.php
@@ -34,16 +34,63 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>로그인</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        input[type="submit"] {
+            background-color: #3498db;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #2980b9;
+        }
+    </style>
 </head>
 <body>
-    <h2>로그인</h2>
+    <h2>Login</h2>
     <form method="post">
-        <label for="username">사용자명:</label>
+        <label for="username">ID:</label>
         <input type="text" name="username" required>
-        <br>
-        <label for="password">비밀번호:</label>
+        <label for="password">Password:</label>
         <input type="password" name="password" required>
-        <br>
         <input type="submit" value="로그인">
     </form>
 </body>
--- register.php
+++ register.php
@@ -21,28 +21,72 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 $conn->close();
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>회원가입</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            text-align: center;
+            padding: 50px;
+        }
+
+        h2 {
+            color: #333;
+        }
+
+        form {
+            max-width: 400px;
+            margin: 0 auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+
+        label {
+            display: block;
+            margin-bottom: 10px;
+            color: #333;
+        }
+
+        input {
+            width: 100%;
+            padding: 10px;
+            margin-bottom: 20px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        input[type="submit"] {
+            background-color: #4caf50;
+            color: #fff;
+            padding: 10px 15px;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #45a049;
+        }
+    </style>
 </head>
 <body>
-    <h2>회원가입</h2>
+    <h2>SignuUp</h2>
     <form method="post">
-        <label for="user_id">아이디:</label>
+        <label for="user_id">ID:</label>
         <input type="text" name="user_id" required>
-        <br>
-        <label for="username">이름:</label>
+        <label for="username">Name:</label>
         <input type="text" name="username" required>
-        <br>
-        <label for="password">비밀번호:</label>
+        <label for="password">Password:</label>
         <input type="password" name="password" required>
-        <br>
         <input type="submit" value="가입 완료">
     </form>
 </body>
 </html>
-
